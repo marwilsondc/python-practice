ee352c891b257de81662a3cff079481578d74d33
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-01 21:20:34 +0800

    submitted program 5

---
+++ prog05.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+first_number = int(input("Input first number: "))
+second_number = int(input("Input second number: "))
+
+print(first_number/second_number)
